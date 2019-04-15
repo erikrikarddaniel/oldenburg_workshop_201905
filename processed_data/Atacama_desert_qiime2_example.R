f2d8baeeb0e90c8e1294d951653b7077aaceadc0
Daniel Lundin <erik.rikard.daniel@gmail.com>
2019-04-15 06:56:00 +0200

    Carina's QIIME2/DADA2 data

---
+++ processed_data/Atacama_desert_qiime2_example.R
@@ -0,0 +1,42 @@
+##########Atacama Qiime2 example for course
+
+#####set wd
+
+setwd("/Users/cbunse/Desktop/CoDa/Qiime2workshop/qiime2-atacama-tutorial/R")
+
+###load libraries
+
+library(tidyverse)
+library(biomformat) ## I used this one instead of biom, but still had some issues installing it
+library(phyloseq)
+library(breakaway)
+
+##import biom dataset
+dat <- read_biom("/Users/cbunse/Desktop/CoDa/Qiime2workshop/qiime2-atacama-tutorial/exported-feature-table/feature-table.biom")
+
+#Coerce dat into a matrix and then into a data frame:
+asv_table <- as.data.frame(as.matrix(biom_data(dat)))
+#get the taxomony
+taxonomy <- observation_metadata(dat)
+
+#### here I get errors, bzw empty data...
+
+
+
+
+#get metadata
+metadata <- sample_metadata(dat)
+## not available in atacama example?
+
+
+############alpha diversity tests (from breakaway example)
+asv_m<- asv_table %>% 
+  #dplyr::select(2:6) %>%
+  remove_rownames()
+
+head(asv_m)
+
+breakaway(asv_m)
+breakaway(apples)
+
+### I don't know how to interpret the results or graph?!?
