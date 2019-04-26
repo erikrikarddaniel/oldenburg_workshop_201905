d0f0d5cf5ef244807e2f3f8c22a89a77b23e8b17
Carina4630 <cbunse@dhcp-81-48.icbm.uni-oldenburg.de>
2019-04-26 10:12:05 +0200

    resolved errors in script, manages to include derep atacama data

--- processed_data/run_dada2_in_r.R
+++ processed_data/run_dada2_in_r.R
@@ -3,9 +3,9 @@
 #install package from bioconductor if required
 #Note, if you are using a Mac (like me), you might have to update R
 #to a version >3.5, otherwise dada2 might give errors while installing.
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("dada2", version = "3.8")
+##if (!requireNamespace("BiocManager", quietly = TRUE))
+##  install.packages("BiocManager")
+##BiocManager::install("dada2", version = "3.8")
 
 library(dada2)
 #library(vegan)
@@ -14,11 +14,10 @@ library(tidyverse)
 packageVersion("dada2")
 
 #citation("dada2") #for credits
-#The below script is modified from the RaukR course
+#The below script is modified from the RaukR course and uses the workshop-atacama example
 #https://nbisweden.github.io/RaukR-2018/metagenomics_John/lab/DADA2_workshop.html
 
 #set working direktory
-setwd("/Users/cbunse/Desktop/CoDa/gitpage/testscripts/MiSeq_Atacama0.01/qiime2-atacama-tutorial/")
 #your data need to be demultiplexed, 
 #                     primers/adapters etc removed, 
 #                     if paired seqs then in matched order
@@ -28,45 +27,39 @@ setwd("/Users/cbunse/Desktop/CoDa/gitpage/testscripts/MiSeq_Atacama0.01/qiime2-a
 #we have to demultiplex them first and we will do that in the terminal/qiime2 way,
 # see https://docs.qiime2.org/2019.1/tutorials/atacama-soils/#
 
-  #load qiime $source activate qiime2-2019.1
+  #load qiime 
   #install the atacama dataset and metafile, rename and store in right folder
       #(see qiime2 tutorial)
   #create EMPPairedEndSequences artefact
   #demultiplex
-  #summarize qiime demux file
-
-  #export demux data to fastqfiles
-
+  #now you can download the demux.qza file
+  # and unzip it: $unzip demux.qza  #this will open all the data
 ###################
 
 # Now in R
 #load and check out the metafile
 
-dataframe <- read.csv("sample-metadata.txt", sep="\t", head=TRUE)
-head(dataframe) # you will realize that the second row does not contain data, so let's remove it
-df<- dplyr::slice(dataframe, 3:n())
-head(df)
-
-#load the demultiplexed data!!
-demux<- read.csv("demux.qza", sep="\t", head=FALSE)
+df <- read.csv("sample-metadata.txt", sep="\t", head=TRUE)
+head(df) 
 
 
 #store the names of the sample names and extract them
-path <- paste(getwd(), "data", sep="/")
-fR1 <- sort(list.files(path, pattern="L1_R1.fastq.gz", full.names = TRUE))
-fR2 <- sort(list.files(path, pattern="L1_R2.fastq.gz", full.names = TRUE))
-sample.names <- regmatches(basename(fR1),regexpr("[0-9]+",basename(fR1)))
-
+path <- paste(getwd(), "/b5d9e3e0-158c-491f-be13-118e9e90588f/data/", sep="/")
+fR1 <- sort(list.files(path, pattern="L001_R1_001.fastq.gz", full.names = TRUE))
+fR2 <- sort(list.files(path, pattern="L001_R2_001.fastq.gz", full.names = TRUE))
+sample.names <- regmatches(basename(fR1),regexpr("[A-Z]+[0-9]+.[0-9]+.[0-9]",basename(fR1))) 
+head(sample.names)
+#regex needs to be adjusted for your datanames of yourse
 
 #trimm and filter
-plotQualityProfile(c(fR1[1],fR2[1]))
+plotQualityProfile(c(fR1[1],fR2[1])) 
 
 # specify the output directory and file names for the filtered reads:
 filt_path <- file.path(path, "filtered")
 filtfR1 <- file.path(filt_path, paste0(sample.names, "_R1_filt.fastq.gz"))
 filtfR2 <- file.path(filt_path, paste0(sample.names, "_R2_filt.fastq.gz"))
 
-filt_trim <- filterAndTrim(fR1, filtfR1, fR2, filtfR2, truncLen=c(200,150), trimLeft = 20,
+filt_trim <- filterAndTrim(fR1, filtfR1, fR2, filtfR2, truncLen=c(140,140), trimLeft = 20,
                            maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
                            compress=TRUE, multithread=TRUE)
 
@@ -105,15 +98,9 @@ getN <- function(x) sum(getUniques(x))
 track <- cbind(filt_trim, sapply(dadaR1, getN), sapply(dadaR2, getN), 
                sapply(mergers, getN), rowSums(seqtab.nochim))
 colnames(track) <- c("input", "filtered", "denoisedF", "denoisedR", "merged", "nonchim")
-datatable(track, options = list(pageLength=24))
+DT::datatable(track, options = list(pageLength=24))
 
 #taxonomy
-system("curl -o data/rdp_species_assignment_16.fa.gz https://zenodo.org/record/801828/files/rdp_species_assignment_16.fa.gz")
-system("curl -o data/rdp_train_set_16.fa.gz https://zenodo.org/record/801828/files/rdp_train_set_16.fa.gz")
-
-taxa <- assignTaxonomy(seqtab.nochim, "data/rdp_train_set_16.fa.gz", multithread=TRUE)
-
-taxa <- addSpecies(taxa, "data/rdp_species_assignment_16.fa.gz")
 
 
 
