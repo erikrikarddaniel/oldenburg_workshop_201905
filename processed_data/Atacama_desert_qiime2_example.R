92080808cbc96c85f18ed3315b981a1dbc5208b8
cbunse <carina.bunse@hifmb.de>
2019-05-01 13:31:26 +0200

    changed scripts on how to load data

--- processed_data/Atacama_desert_qiime2_example.R
+++ processed_data/Atacama_desert_qiime2_example.R
@@ -1,33 +1,32 @@
-##########Atacama Qiime2 example for course
+##########Atacama Qiime2 example 
+#after running the qiime2biom2tsv.md file, 
+#you can import the data like this as well.
+
 
 ###load libraries
 
-library(tidyverse)
-library(biomformat) ## I used this one instead of biom, but still had some issues installing it
-library(phyloseq)
-library(breakaway)
+library(readr)
+library(dplyr)
+library(tidyr)
 
-##import biom dataset
-dat <- read_biom("feature-table.biom")
+asvs <- read_tsv(
+  'feature-table.tsv', 
+  col_types = cols(.default = col_double(), `#OTU ID` = col_character()), 
+  skip = 1
+) %>% 
+  rename(asv_id = `#OTU ID`) %>% 
+  gather(sample, count, 2:ncol(.)) %>% 
+  filter(count > 0) %>% 
+  mutate(count = as.integer(count))
 
-#Coerce dat into a matrix and then into a data frame:
-asv_table <- as.data.frame(as.matrix(biom_data(dat)))
+#unzip the taxonomy file taxonomy.qza
 #get the taxomony
-taxonomy <- observation_metadata(dat)
+taxonomy <- read_tsv( 'taxonomy.tsv')  %>% 
+    rename(asv_id =`Feature ID`)
+
 
 
 #get metadata
-metadata <- sample_metadata(dat)
-## not available in atacama example?
+metadata <- read_tsv('metadata.yml')
 
-
-############alpha diversity tests (from breakaway example)
-asv_m<- asv_table %>% 
-  #dplyr::select(2:6) %>%
-  remove_rownames()
-
-head(asv_m)
-
-breakaway(asv_m)
-breakaway(apples)
 
